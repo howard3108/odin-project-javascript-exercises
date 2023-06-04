46e562a596644cc229c55a94b8f3930d52703fa6
Howard Chen <52931162+howard3108@users.noreply.github.com>
2023-06-04 19:58:05 -0400

    add: added solution 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    // sum of first two number
+    // given the num find the num sum
+    // 1 1 2 3 5 8 13
+    let num1 = 0;
+    let num2 = 1;
+    if (num <= 0) {return "OOPS";}
+    for (let i = 1; i < num; i++) {
+        const temp = num2;
+        num2 += num1;
+        num1 = temp;
+    }
+    return num2;
 };
 
 // Do not edit below this line
