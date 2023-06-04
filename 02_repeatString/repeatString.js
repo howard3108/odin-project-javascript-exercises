c56a344c068da56c1734b0547b3569c908156996
Howard Chen <52931162+howard3108@users.noreply.github.com>
2023-06-04 13:26:14 -0400

    Added solution for 01 and 02 exercise.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, times) {
+    let newString = "";
+    if (times < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < times; i++) {
+        newString += string;
+    }
+    return newString;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
