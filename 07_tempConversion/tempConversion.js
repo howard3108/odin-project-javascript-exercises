a370ba22d727d7ebfcfb0886dd1f8a227e0489be
Howard Chen <52931162+howard3108@users.noreply.github.com>
2023-06-04 17:22:18 -0400

    add: added solution for exercise 07 and 08.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  // (degree - 32) * 5 / 9
+  return Math.round((degree - 32) * 5 / 9 * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  // (degree × 9/5) + 32
+  return Math.round(((degree * 9 / 5) + 32) * 10) /10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(addNum1, addNum2) {
+	return addNum1 + addNum2;
 };
 
-const subtract = function() {
-	
+const subtract = function(subNum1, subNum2) {
+	return subNum1 - subNum2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+    let newSum = 0;
+	array.forEach(element => {
+        newSum += element;
+    })
+    return newSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+    let multiplyResult = 1;
+    array.forEach(element => {
+        multiplyResult *= element;
+    })
+    return multiplyResult;
 };
 
-const power = function() {
-	
+const power = function(num, powerOf) {
+    let answer = num;
+    for (let i = 1; i < powerOf; i++) {
+        answer *= num;
+    }
+    return answer;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    let i = 0;
+    let answer = 1;
+    if (num === 0) {
+        return 1;
+    }
+    do {
+        i++;
+        answer *= i;
+    } while (i < num);
+    return answer;
 };
 
 // Do not edit below this line
