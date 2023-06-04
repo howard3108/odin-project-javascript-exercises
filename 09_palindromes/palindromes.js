680ef2dd6399d86d4c2154e0a1dd37f53a07c612
Howard Chen <52931162+howard3108@users.noreply.github.com>
2023-06-04 18:21:08 -0400

    add: exercise 9 solution.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    // remove all spaces and other character
+    const newString = string.toLowerCase().replace(/[^a-z0-9]/g, '');
+    // check first char and last char
+    const newStringLen = newString.length;
+    const midStr = Math.floor(newStringLen / 2);
+    let answer = false;
+    // if length is odd number find the middle char   (0)stats(4) midStr = 2
+    for (let i = 0; i < midStr; i++) {
+        answer = newString.charAt(i) === newString.charAt(newStringLen - 1 - i);
+    }
+    return answer;
 };
 
 // Do not edit below this line
