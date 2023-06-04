5431dc13e07267bf004c2b85c3f2c20c717de6cf
Howard Chen <52931162+howard3108@users.noreply.github.com>
2023-06-04 15:26:25 -0400

    add: solutions 5 and 6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    let sum = 0;
+    if (min > max) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+    let i = min;
+    if (min >= 0 && max >= 0 ) {
+        for (i; i <= max; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 
 };
 
