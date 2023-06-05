7530ed65ffaf250a34cb5b03ee2e58b9f96f7973
Howard Chen <52931162+howard3108@users.noreply.github.com>
2023-06-05 15:41:13 -0400

    add: exercise 12 solution.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, current) => {
+        if (!oldest.yearOfDeath) {
+            oldest.yearOfDeath = new Date().getFullYear();
+        } else if (!current.yearOfDeath) {
+            current.yearOfDeath = new Date().getFullYear();
+        }
+        const getAgeOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+        const getAgeCurrent = current.yearOfDeath - current.yearOfBirth;
+        return (getAgeOldest > getAgeCurrent) ? oldest : current;
+    })
 };
 
 // Do not edit below this line
